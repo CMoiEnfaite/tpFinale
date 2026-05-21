041aa7d465242e5cef089c266307a2af71051d55
CmoiEnft <bourcier.lukas44@gmail.com>
2026-05-21 16:02:28 +0200

    config app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Register } from './pages/register/register';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home, title: 'Home'},
+    {path: 'register', component: Register, title: 'Inscription'},
+    {path: 'login', component: Login, title: 'Connexion'},
+    {path: '**', redirectTo: '', pathMatch: 'full'}
+];
